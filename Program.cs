2ee8fca3005f15fead93c18ebd0030932cbd0397
DESKTOP-90LV02M\admin <leducnamtek123@gmail.com>
2024-08-02 10:21:42 +0700

    app.UseDeveloperExceptionPage();

--- Program.cs
+++ Program.cs
@@ -69,7 +69,7 @@ app.UseStaticFiles();
 app.UseRouting();
 
 app.UseAuthorization();
-
+app.UseDeveloperExceptionPage();
 app.MapControllerRoute(
     name: "default",
     pattern: "{controller=Home}/{action=Index}/{id?}");
