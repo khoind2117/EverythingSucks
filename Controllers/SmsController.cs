b6e5e76772acc74efd67055e0c4ec6ab78eb4694
Nguyễn Đăng Khôi <khoind2117@gmail.com>
2024-08-08 20:41:28 +0700

    2FA Twilio, Admin logout

---
+++ Controllers/SmsController.cs
@@ -0,0 +1,32 @@
+﻿using EverythingSucks.Services;
+using Microsoft.AspNetCore.Mvc;
+using Twilio.Clients;
+using Twilio.Rest.Api.V2010.Account;
+using Twilio.Types;
+
+namespace EverythingSucks.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class SmsController : ControllerBase
+    {
+        private readonly ITwilioRestClient _client;
+
+        public SmsController(ITwilioRestClient client)
+        {
+            _client = client;
+        }
+
+        [HttpPost]
+        [HttpPost("send-sms")]
+        public async Task<IActionResult> SendSms(SmsMessage model)
+        {
+            var message = MessageResource.Create(
+                to: new PhoneNumber(model.To),
+                from: new PhoneNumber(model.From),
+                body: model.Message,
+                client: _client);
+            return Ok("Success" + message.Sid);
+        }
+    }
+}
---
+++ ViewModels/VerifyCodeViewModel.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace EverythingSucks.ViewModels
+{
+    public class VerifyCodeViewModel
+    {
+        [Required]
+        [Display(Name = "Verification Code")]
+        public string Code { get; set; }
+
+        public string? ReturnUrl { get; set; }
+    }
+}
