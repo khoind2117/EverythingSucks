9585c566489d4f87b93ece9cac10b0b50eecf5be
Nguyễn Đăng Khôi <khoind2117@gmail.com>
2024-08-12 01:59:09 +0700

    MailchimpExport, CsvHelper

---
+++ ViewModels/MailchimpResponse.cs
@@ -0,0 +1,10 @@
+﻿using Newtonsoft.Json;
+
+namespace EverythingSucks.ViewModels
+{
+    public class MailchimpResponse
+    {
+        [JsonProperty("members")]
+        public List<Member> Members { get; set; }
+    }
+}
---
+++ ViewModels/Member.cs
@@ -0,0 +1,10 @@
+﻿using Newtonsoft.Json;
+
+namespace EverythingSucks.ViewModels
+{
+    public class Member
+    {
+        [JsonProperty("email_address")]
+        public string EmailAddress { get; set; }
+    }
+}
